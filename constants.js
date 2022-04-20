fc751a5c9ddd39dbc29366183a2a3a9c93bde4f1
Maor Roizman Gheiler <maor.roizman@utec.edu.pe>
2022-04-20 16:30:19 -0500

    New channel

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const DISCORD_CHANNEL_ID = '966182400130445312';
+const DISCORD_CHANNEL_ID = '961752223694585926';
 const CANVAS_BASE_URI = 'https://utec.instructure.com/api/v1/courses'
 const CANVAS_ANNOUNCEMENTS_URI = 'discussion_topics?is_announcement=true'
 const CANVAS_COURSES_IDS=['9140', '9147', '9154', '9161']
